903e44d62a57de0cb613a31db87cc9f26eb14bd4
dinhdinh02 <dinhn1182@gmail.com>
2024-05-08 20:52:25 +0700

    push

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase-admin/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSjcBsYQTG_3hKotYYz4sdjyr8EgnSEoA",
+  authDomain: "note-app-holetex-d00a3.firebaseapp.com",
+  projectId: "note-app-holetex-d00a3",
+  storageBucket: "note-app-holetex-d00a3.appspot.com",
+  messagingSenderId: "343747847234",
+  appId: "1:343747847234:web:d4ecc557528be9cde779db",
+  measurementId: "G-87HKP2ZTWW"
+};
+
+initializeApp(firebaseConfig);
